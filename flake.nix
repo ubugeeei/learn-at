26f5c890e4ef8897ded065f832c5c89c3ce0992d
ubugeeei <ubuge1122@gmail.com>
2026-07-10 13:12:51 +0900

    chore: add reproducible Nix development shell

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Dependency-free Scala 3 AT Protocol hands-on";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShellNoCC {
+          packages = [
+            pkgs.jdk21
+            pkgs.sbt
+          ];
+
+          JAVA_HOME = "${pkgs.jdk21}";
+        };
+      });
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+    };
+}
+
